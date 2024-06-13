810d416b934e2144d57994dd4a90897bd52ba805
Adyem <Bastianvangenechten@hotmail.com>
2024-06-13 14:00:51 +0200

    adding beginnings of h file

---
+++ cubed.h
@@ -0,0 +1,14 @@
+#ifndef CUBED_H
+# define CUBED_H
+
+# include "minilibx-linux/mlx.h"
+# include "libft/libft/libft.h"
+# include "libft/printf/ft_printf.h"
+# include "libft/printf_fd/ft_printf_fd.h"
+# include "libft/get_next_line/get_next_line.h"
+
+# ifndef DEBUG
+#  define DEBUG 0
+# endif
+
+#endif
