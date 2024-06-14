4ff7059171217c46f1e4ed7459266543b78b3e57
Bastian Vangenechten <Bastianvangenechten@hotmail.com>
2024-06-15 00:11:39 +0200

    changing free memory

--- free_memory.c
+++ free_memory.c
@@ -44,6 +44,10 @@ void	ft_free_info(t_cubed *info)
 	ft_free_double_char(info->map.content);
 	ft_free_double_char(info->map.map);
 	ft_free_double_char(info->map.copy_map);
+	free(info->colors.floor_string);
+	free(info->colors.ceiling_string);
+	ft_free_double_char(info->colors.floor_array);
+	ft_free_double_char(info->colors.ceiling_array);
 	ft_free_mlx(info);
 	free(info);
 }
