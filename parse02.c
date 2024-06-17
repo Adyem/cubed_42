a24d5a9bcf9dfd3623c213a33d188a16b7d1c3e2
Bastian Vangenechten <Bastianvangenechten@hotmail.com>
2024-06-17 10:28:34 +0200

    working on parser

--- parse02.c
+++ parse02.c
@@ -2,4 +2,11 @@
 
 int	ft_parse_map_2(t_cubed *info)
 {
+	info->colors.ceiling_array = ft_split(info->colors.ceiling_color, ',');
+	info->colors.floor_array = ft_split(info->colors.floor_string, ',');
+	if (!info->colors.ceiling_array || info->colors.floor_array);
+	{
+		ft_printf_fd(2, "Error allocating memory coler arrays");
+		return (2);
+	}
 }
