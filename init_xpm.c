6e13cdb54ba1a1341667888f051e39e713a87d33
Bastian Vangenechten <Bastianvangenechten@hotmail.com>
2024-08-23 22:24:15 +0200

    fixing to small image crash

--- init_xpm.c
+++ init_xpm.c
@@ -31,6 +31,30 @@ void	texture_to_array(int *address, int i, t_cubed *info)
 	}
 }
 
+int	ft_check_texture(int height, int width)
+{
+	if (TEX_HEIGHT != TEX_WIDTH)
+		return (1);
+	if (height != TEX_HEIGHT)
+		return (1);
+	if (width != TEX_WIDTH)
+		return (1);
+	return (0);
+}
+
+int	ft_check(int check, int height, int width)
+{
+	if (check == 1 && ft_check_texture(height, width))
+		return (1);
+	if (check == 2 && ft_check_texture(height, width))
+		return (1);
+	if (check == 3 && ft_check_texture(height, width))
+		return (1);
+	if (check == 4 && ft_check_texture(height, width))
+		return (1);
+	return (0);
+}
+
 void	init_xpm_b(t_cubed *info, int tmp_bit, int tmp_len, int tmp_end)
 {
 	int		*address;
@@ -38,7 +62,8 @@ void	init_xpm_b(t_cubed *info, int tmp_bit, int tmp_len, int tmp_end)
 	info->textures.south_texture.img_ptr = mlx_xpm_file_to_image(info->mlx,
 			info->textures.south_texture.file_name,
 			&(info->textures.tex_w), &(info->textures.tex_h));
-	if (!info->textures.south_texture.img_ptr)
+	if (!info->textures.south_texture.img_ptr
+			|| ft_check(1, info->textures.tex_h, info->textures.tex_w))
 		destroy_error(info, 3);
 	address = (int *)mlx_get_data_addr(info->textures.south_texture.img_ptr,
 			&tmp_bit, &tmp_len, &tmp_end);
@@ -48,7 +73,8 @@ void	init_xpm_b(t_cubed *info, int tmp_bit, int tmp_len, int tmp_end)
 	info->textures.west_texture.img_ptr = mlx_xpm_file_to_image(info->mlx,
 			info->textures.west_texture.file_name,
 			&(info->textures.tex_w), &(info->textures.tex_h));
-	if (!info->textures.west_texture.img_ptr)
+	if (!info->textures.west_texture.img_ptr
+			|| ft_check(2, info->textures.tex_h, info->textures.tex_w))
 		destroy_error(info, 3);
 	address = (int *)mlx_get_data_addr(info->textures.west_texture.img_ptr,
 			&tmp_bit, &tmp_len, &tmp_end);
@@ -64,7 +90,8 @@ void	init_xpm(t_cubed *info, int tmp_bit, int tmp_len, int tmp_end)
 	info->textures.north_texture.img_ptr = mlx_xpm_file_to_image(info->mlx,
 			info->textures.north_texture.file_name,
 			&(info->textures.tex_w), &(info->textures.tex_h));
-	if (!info->textures.north_texture.img_ptr)
+	if (!info->textures.north_texture.img_ptr
+			|| ft_check(3, info->textures.tex_w, info->textures.tex_h))
 		destroy_error(info, 3);
 	address = (int *)mlx_get_data_addr(info->textures.north_texture.img_ptr,
 			&tmp_bit, &tmp_len, &tmp_end);
@@ -74,7 +101,8 @@ void	init_xpm(t_cubed *info, int tmp_bit, int tmp_len, int tmp_end)
 	info->textures.east_texture.img_ptr = mlx_xpm_file_to_image(info->mlx,
 			info->textures.east_texture.file_name,
 			&(info->textures.tex_w), &(info->textures.tex_h));
-	if (!info->textures.east_texture.img_ptr)
+	if (!info->textures.east_texture.img_ptr
+			|| ft_check(4, info->textures.tex_h, info->textures.tex_w))
 		destroy_error(info, 3);
 	address = (int *)mlx_get_data_addr(info->textures.east_texture.img_ptr,
 			&tmp_bit, &tmp_len, &tmp_end);
